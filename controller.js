9fe3641e9d1291fbd4937e6add163ace4ebbd2d4
Leo Arias <yo@elopio.net>
2017-08-30 01:05:15 -0600

    Agregué el amuleto

---
+++ controller.js
@@ -0,0 +1,9 @@
+var teleport = document.getElementById("teleport")
+
+teleport.addEventListener("click", function() {
+    console.log("click on teleport!")
+    var image_360_1 = document.getElementById("image-360-1")
+    image_360_1.setAttribute("opacity", "0")
+    var image_360_2 = document.getElementById("image-360-2")
+    image_360_2.setAttribute("opacity", "1")
+})
