e92163e5485f6a752d60525d4454140df53454f7
Leo Arias <yo@elopio.net>
2017-08-31 09:41:34 -0600

    Arreglé el transportador

--- controller.js
+++ controller.js
@@ -1,9 +1,15 @@
-var teleport = document.getElementById("teleport")
+var teleport = document.getElementById("teleport");
 
 teleport.addEventListener("click", function() {
-    console.log("click on teleport!")
-    var image_360_1 = document.getElementById("image-360-1")
-    image_360_1.setAttribute("opacity", "0")
-    var image_360_2 = document.getElementById("image-360-2")
-    image_360_2.setAttribute("opacity", "1")
-})
+    console.log("click on teleport!");
+    var image_360_1 = document.getElementById("image-360-1");
+    image_360_1.setAttribute("opacity", "0");
+    var image_360_2 = document.getElementById("image-360-2");
+    image_360_2.setAttribute("opacity", "1");
+});
+
+var khipukamayuqAnimation = document.getElementById("khipukamayuq-animation");
+
+khipukamayuqAnimation.addEventListener("animationend", function () {
+    document.querySelector('#amulet').emit("khipukamayuqVisible");
+});
