fc0f4f78b4631cb5c1c23c3e501842410060bccf
Leo Arias <yo@elopio.net>
2021-03-21 20:32:38 -0600

    Added the intro scene

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,8 +3,17 @@ AFRAME.registerComponent("instructions", {
     let scene = this.el.sceneEl;
     let instructions = scene.querySelector("#instructions");
     instructions.addEventListener("animationtimelinecomplete", function() {
-      let skyInstructions = instructions.querySelector("#sky-instructions");
       instructions.setAttribute("visible", false);
+    });
+  }
+});
+
+AFRAME.registerComponent("intro", {
+  init: function () {
+    let scene = this.el.sceneEl;
+    let intro = scene.querySelector("#intro");
+    intro.addEventListener("animationtimelinecomplete", function() {
+      intro.setAttribute("visible", false);
       let scene1 = scene.querySelector("#scene1");
       scene1.setAttribute("visible", true);
     });
@@ -12,12 +21,17 @@ AFRAME.registerComponent("instructions", {
 });
 
 
-
 var start = function() {
   // Hide the play button.
   var rootPlay = document.getElementById("root");
   rootPlay.style.display = "none";
 
-  instructionsHeadset = document.getElementById("instructions-headset");
-  instructionsHeadset.emit("startInstructions");
+  //  instructionsHeadset = document.getElementById("instructions-headset");
+  //  instructionsHeadset.emit("startInstructions");
+
+  // For debugging.
+  // Start intro Scene.
+  let intro = document.getElementById("intro");
+  intro.setAttribute("visible", true);
+  intro.emit("startIntro");
 };
