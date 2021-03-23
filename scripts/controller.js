bb51bc8a74e588458ce6b9e8e01d990067ee6679
Leo Arias <yo@elopio.net>
2021-03-22 21:11:05 -0600

    Finish scene1

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,12 +1,13 @@
+var teleportEnabled = false;
+
+
 AFRAME.registerComponent("instructions", {
   init: function () {
     let scene = this.el.sceneEl;
     let instructions = scene.querySelector("#instructions");
     instructions.addEventListener("animationtimelinecomplete", function() {
       instructions.setAttribute("visible", false);
-      let intro = scene.querySelector("#intro");
-      intro.setAttribute("visible", true);
-      intro.emit("startIntro");
+      startIntro();
     });
   }
 });
@@ -17,13 +18,7 @@ AFRAME.registerComponent("intro", {
     let intro = scene.querySelector("#intro");
     intro.addEventListener("animationtimelinecomplete", function() {
       intro.setAttribute("visible", false);
-      let scene1 = scene.querySelector("#scene1");
-      scene1.setAttribute("visible", true);
-      scene1.emit("startScene1");
-      let soundBackgroundScene1 = scene.querySelector("#sound-background-scene1");
-      soundBackgroundScene1.components.sound.playSound();
-      let camera = scene.querySelector("#camera");
-      camera.setAttribute("target-indicator", "target: #scene1-teleport");
+      startScene1();
     });
   }
 });
@@ -31,8 +26,23 @@ AFRAME.registerComponent("intro", {
 AFRAME.registerComponent("scene1", {
   init: function () {
     let scene = this.el.sceneEl;
+
+    // After the background sound volume is reduced, play the sound effect.
+    let scene1SoundEffect = scene.querySelector("#scene1-sound-effect");
+    let scene1SoundBackground = scene.querySelector("#scene1-sound-background");
+    scene1SoundBackground.addEventListener("animationcomplete__volume_reduce", function() {
+      scene1SoundEffect.components.sound.playSound();
+    });
+
+    // After the sound effect ends, raise the background sound volume.
+    scene1SoundEffect.addEventListener("sound-ended", function() {
+      scene1SoundBackground.emit("scene1_volume_raise");
+    });
+
+    // After the move animation is complete.
     let scene1 = scene.querySelector("#scene1");
     scene1.addEventListener("animationtimelinecomplete", function() {
+      //  Pause the gifs
       let gifAnaoWalk = scene.querySelector("#scene1-gif-anao-walk");
       let gifAnapWalk = scene.querySelector("#scene1-gif-anap-walk");
       let gifConiWalk = scene.querySelector("#scene1-gif-coni-walk");
@@ -41,7 +51,31 @@ AFRAME.registerComponent("scene1", {
       gifAnapWalk.pause();
       gifConiWalk.pause();
       gifDanieWalk.pause();
+
+      teleportEnabled = true;
+
+      // Mark the teleport as the target.
+      let camera = scene.querySelector("#camera");
+      camera.setAttribute("target-indicator", "target: #scene1-teleport");
     });
+
+    teleport = scene.querySelector("#scene1-image-teleport");
+    teleport.addEventListener("click", function() {
+      console.log("click on teleport!");
+      if (teleportEnabled == true) {
+        teleportEnabled = false;
+
+        scene1.setAttribute("visible", false);
+        let scene1SoundBackground = scene.querySelector("#scene1-sound-background");
+        scene1SoundBackground.components.sound.stopSound();
+
+        let camera = scene.querySelector("#camera");
+        camera.removeAttribute("target-indicator");
+        // scene2.setAttribute("visible", true);
+        // scene2.emit("startScene2");
+      }
+    });
+
   }
 });
 
@@ -51,19 +85,26 @@ var start = function() {
   var rootPlay = document.getElementById("root");
   rootPlay.style.display = "none";
 
-  instructionsHeadset = document.getElementById("instructions-headset");
-  instructionsHeadset.emit("startInstructions");
-
+  //startInstructions();
   // For debugging.
-  // Start intro scene.
-  //let intro = document.getElementById("intro");
-  //intro.setAttribute("visible", true);
-  //intro.emit("startIntro");
+  startScene1();
+};
 
-  // Start scene 1, medialab.
-  //let scene1 = document.getElementById("scene1");
-  //scene1.setAttribute("visible", true);
-  //scene1.emit("startScene1");
-  //let soundBackgroundScene1 = document.getElementById("sound-background-scene1");
-  //soundBackgroundScene1.components.sound.playSound();
+var startInstructions = function() {
+  let instructionsHeadset = document.getElementById("instructions-headset");
+  instructionsHeadset.emit("startInstructions");
+};
+
+var startIntro = function() {
+  let intro = document.getElementById("intro");
+  intro.setAttribute("visible", true);
+  intro.emit("startIntro");
+};
+
+var startScene1 = function() {
+  let scene1 = document.getElementById("scene1");
+  scene1.setAttribute("visible", true);
+  scene1.emit("startScene1");
+  let scene1SoundBackground = document.getElementById("scene1-sound-background");
+  scene1SoundBackground.components.sound.playSound();
 };
